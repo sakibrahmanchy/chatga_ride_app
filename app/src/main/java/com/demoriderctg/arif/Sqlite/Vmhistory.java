d2acdd08fa164412cf9764651f25023a1f23485e
sakibrahmancuet <sakib.cse11.cuet@gmail.com>
2017-12-06 23:55:49 +0600

    Registration

---
+++ app/src/main/java/com/demoriderctg/arif/Sqlite/Vmhistory.java
@@ -0,0 +1,13 @@
+package com.demoriderctg.arif.Sqlite;
+
+/**
+ * Created by Arif on 11/22/2017.
+ */
+
+public class Vmhistory {
+    public String locationName;
+   public double letitude;
+   public double longitude;
+  public   int id;
+  public   String searchDate;
+}
