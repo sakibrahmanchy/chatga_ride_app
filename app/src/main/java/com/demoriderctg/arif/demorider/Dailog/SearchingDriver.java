902be6ef6df9a3623ed61ed77412410c3e63fc6d
ariful islam <arifulcuet52@gmail.com>
2018-01-10 23:32:08 +0600

    Bugg Fixed

--- app/src/main/java/com/demoriderctg/arif/demorider/Dailog/SearchingDriver.java
+++ app/src/main/java/com/demoriderctg/arif/demorider/Dailog/SearchingDriver.java
@@ -1,6 +1,7 @@
 package com.demoriderctg.arif.demorider.Dailog;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -10,11 +11,38 @@ import com.demoriderctg.arif.demorider.R;
 
 public class SearchingDriver extends AppCompatActivity {
 
+
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searching_driver);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus <=100) {
+                    progressStatus += 1;
+                    // Update the progress bar and display the
+                    //current value in the text view
+                    handler.post(new Runnable() {
+                        public void run() {
+                            if(progressStatus==50){
+                                Intent  intent = new Intent(SearchingDriver.this,MapActivity.class);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                    try {
+                        // Sleep for 200 milliseconds.
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
