6e32ef75beb598dd4ae927162ba94cd5dfbeb8e0
Jobayer sheikh <jobayersheikhbd@gmail.com>
2018-01-24 01:47:19 +0600

    Small fix

--- app/src/main/java/com/demoriderctg/arif/demorider/MainActivity.java
+++ app/src/main/java/com/demoriderctg/arif/demorider/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "Mainactivity";
     private static final int ERROR_DIALOG_REQUEST = 9001;
-    public static boolean check = false;
+    public static boolean check = true;
     private SharedPreferences pref;
     private ConnectionCheck connectionCheck;
 
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         connectionCheck = new ConnectionCheck(this);
-        new CurrentDateTime(this).getCurrentDateTime();
+        //new CurrentDateTime(this).getCurrentDateTime();
         if (!connectionCheck.isNetworkConnected()) {
             Intent intent = new Intent(this, InternetCheckActivity.class);
             startActivityForResult(intent, AppConstant.INTERNET_CHECK);
