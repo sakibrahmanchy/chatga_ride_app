99a937686fa37c19b7aa4bc3a97607bf0f459b91
ariful islam <arifulcuet52@gmail.com>
2018-01-28 20:59:00 +0600

    Merge branch 'master' of https://gitlab.com/Joint.Venture/devClient
    # Conflicts:
    #	app/src/main/java/__Firebase/FirebaseResponse/InitialAcceptanceOfRideResponse.java

---
+++ app/src/main/java/com/demoriderctg/arif/demorider/Setting/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.demoriderctg.arif.demorider.Setting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.demoriderctg.arif.demorider.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
