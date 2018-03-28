7d42a84032626580822571269759b5ae5b054e5d
ariful islam <arifulcuet52@gmail.com>
2018-03-29 02:22:42 +0600

    Merge branch 'master' of https://gitlab.com/Joint.Venture/devClient
    # Conflicts:
    #	.idea/modules.xml
    #	app/src/main/res/layout/map_activity.xml

---
+++ app/src/main/java/com/demoriderctg/arif/demorider/Refarrel/Refarrel.java
@@ -0,0 +1,15 @@
+package com.demoriderctg.arif.demorider.Refarrel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.demoriderctg.arif.demorider.R;
+
+public class Refarrel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refarrel);
+    }
+}
